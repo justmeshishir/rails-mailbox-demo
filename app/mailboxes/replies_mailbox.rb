703dec9c92bcde6d2637b77d6f8bffc2544dd5aa
shishir <shishirthapa1@gmail.com>
2019-10-16 15:30:41 +0545

    send and display replies

---
+++ app/mailboxes/replies_mailbox.rb
@@ -0,0 +1,34 @@
+class RepliesMailbox < ApplicationMailbox
+  MATCHER = /reply-(.+)@rails-mailbox-demo.com/i
+  # mail => Mail object
+  # inbound_email => ActionMailbox::InboundEmail record
+  #
+  # before_processing :ensure_user
+  def process
+    return if user.nil?
+
+    discussion.comments.create(
+       user: user,
+       body: mail.decoded
+    )
+  end
+
+  def user
+    @user ||= User.find_by(email: mail.from)
+  end
+
+  def discussion
+    @discussion ||= Discussion.find(discussion_id)
+  end
+
+  def discussion_id
+    recipient = mail.recipients.find { |r| MATCHER.match?(r) }
+    recipient[MATCHER, 1]
+  end
+
+  # def ensure_user
+  #   if user.nil?
+  #     bounce_with UserMailer.missing(inbound_emai)
+  #   end
+  # end
+end
